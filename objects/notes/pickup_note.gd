9e188e2ed3d9f76e998a8c1b354db7c9e66cd353
JakeItEasy <jhoffmann982@gmail.com>
2024-03-14 14:02:43 -0400

    functional notes!

--- objects/notes/pickup_note.gd
+++ objects/notes/pickup_note.gd
@@ -1,6 +1,12 @@
 extends Area3D
 
 @onready var pickup_sound = $"../PickupSound"
+@onready var note_UI = $"../NoteLayer"
 
 func use():
-	get_parent().queue_free()
+	note_UI.visible = true
+	await get_tree().create_timer(1.0).timeout  
+	
+func _process(delta):
+	if note_UI.visible == true and Input.is_action_just_pressed("escape"):
+		note_UI.visible = false 
