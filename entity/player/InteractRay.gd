08418e6232c158686c894f0a476d2c966c57ba70
JakeItEasy <jhoffmann982@gmail.com>
2024-03-22 09:45:41 -0400

    Clothman model now in the game!!!

--- entity/player/InteractRay.gd
+++ entity/player/InteractRay.gd
@@ -1,8 +1,7 @@
 extends Camera3D
 
 func _process(delta : float) -> void:
-	if Input.is_action_just_pressed("interact"):
-		if $InteractRay.is_colliding():
-			var collider = $InteractRay.get_collider()
-			if collider.is_in_group("interactable"):
-				collider.use()
+	if Input.is_action_just_pressed("interact") and $InteractRay.is_colliding():
+		var collider = $InteractRay.get_collider()
+		if collider.is_in_group("interactable"):
+			collider.use()
