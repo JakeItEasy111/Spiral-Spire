ea166d4acc23ca7e4ac4590b616259a6eb8ce49c
JakeItEasy <jhoffmann982@gmail.com>
2024-04-09 14:03:14 -0400

    WE ARE LIVING IN THE LIGHT
    clothman to do:
    - sounds
    - burst attack?
    - death animation (dust?)

--- entity/enemy/clothman/clothman_projectile.gd
+++ entity/enemy/clothman/clothman_projectile.gd
@@ -1,14 +1,19 @@
 extends Area3D
 
 var dir = Vector3()
+var kill = false 
+@onready var proj_anim = $ProjectileSprite
+@onready var proj_kill_anim = $BurstEffect
+
 const BULLET_SPEED = 6
 
 func _ready() -> void:
 	top_level = true; 
-	$AnimatedSprite3D.play()
+	proj_anim.play()
 
 func _process(delta):
-	position -= transform.basis.z * delta * BULLET_SPEED
+	if !kill:
+		position -= transform.basis.z * delta * BULLET_SPEED
 
 func _on_BulletTimer_timeout() -> void:
 	queue_free() 
@@ -17,4 +22,16 @@ func _on_body_entered(body):
 	if body.is_in_group("player"): 
 		var dir = body.global_transform.origin - global_transform.origin
 		body.hit(dir, 10)
+		kill_effect() 
+		await get_tree().create_timer(0.75).timeout
 		queue_free() 
+	elif !body.is_in_group("entity"):
+		kill_effect() 
+		await get_tree().create_timer(0.75).timeout
+		queue_free()
+
+func kill_effect():
+	kill = true 
+	proj_anim.visible = false 
+	proj_kill_anim.visible = true 
+	proj_kill_anim.play()
