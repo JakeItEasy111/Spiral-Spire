40f07fd0dd09733a7521f5bfeef6ec7d140c92df
JakeItEasy <jhoffmann982@gmail.com>
2024-04-23 11:31:45 -0400

    spiral level one complete

--- levels/dungeons/spiral.gd
+++ levels/dungeons/spiral.gd
@@ -1,12 +1,14 @@
 extends Node
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	$AudioStreamPlayer.play()
+	
 func _on_timer_timeout():
 	$NavigationRegion3D/GridMap.visible = false
 	await get_tree().create_timer(0.2).timeout
 	$NavigationRegion3D/GridMap.visible = true
 	$Timer.wait_time = randi() % 5
+
+func _on_area_3d_area_entered(area):
+	$AudioStreamPlayer.stop()
+	get_tree().current_scene.get_node("SceneTransition").change_level_scene(self, "res://levels/dungeons/spiral_two.tscn")
