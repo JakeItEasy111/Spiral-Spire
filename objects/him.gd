735b2469101e9e5b2831abe5b53032311b4a8f68
JakeItEasy <jhoffmann982@gmail.com>
2024-04-30 13:39:34 -0400

    v1.0.0.2
    - updated note text in spiral_two
    - floor 2 tweaks

--- objects/him.gd
+++ objects/him.gd
@@ -6,6 +6,15 @@ extends StaticBody3D
 func destroy():
 	visible = false
 	break_sound.play()
+	$Timer.start()
 	await break_sound.finished
 	get_tree().change_scene_to_file("res://levels/credits/credits.tscn")
 	
+
+
+func _on_timer_timeout():
+	if(visible == true):
+		visible = false
+	else:
+		visible = true
+	$Timer.start() 
